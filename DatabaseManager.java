8b4bfba3882afb2a0e2d542963e10967b2cc81a2
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-04-30 08:25:15 +0530

    Database connection setup

--- DatabaseManager.java
+++ DatabaseManager.java
@@ -1,1 +1,12 @@
+package src;
 
+import java.sql.*;
+
+public class DatabaseManager {
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/db_java";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "2407";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+    }
