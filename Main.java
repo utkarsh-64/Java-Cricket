830a64fe1784354463c00f088db0cccc0ab869d3
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-04-30 08:59:44 +0530

    Adding Entry Form Components and Start scene layout

--- Main.java
+++ Main.java
@@ -28,3 +28,25 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Entry form components
+        TextField nameField = new TextField();
+        nameField.setPromptText("Enter your name");
+        TextField oversField = new TextField();
+        oversField.setPromptText("Enter number of overs");
+        Button startButton = new Button("Start Game");
+
+        // Start scene layout
+        VBox startLayout = new VBox(10,
+                new Label("Welcome to the Cricket Game!"),
+                nameField,
+                oversField,
+                startButton,
+                new Separator(),
+                searchField,
+                new HBox(10, searchButton, deleteButton)
+        );
+        startLayout.setStyle("-fx-padding: 20;");
+        Scene startScene = new Scene(startLayout, 400, 300);
